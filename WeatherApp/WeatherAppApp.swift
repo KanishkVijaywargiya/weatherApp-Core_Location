506fa9bfd4d237b3bf4ae65a03b26ffb020de91b
KanishkVijaywargiya <secretcodersociety@gmail.com>
2023-01-03 17:33:06 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Kanishk Vijaywargiya on 03/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
